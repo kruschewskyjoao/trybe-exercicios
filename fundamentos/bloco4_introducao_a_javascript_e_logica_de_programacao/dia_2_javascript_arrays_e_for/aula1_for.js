cd264cd7d1b15c77d440f5355a7293309d9f32f4
joaokruschewsky <joaokruschewsky@gmail.com>
2021-11-29 15:57:02 -0300

    Terminando os exemplos da aula dia 2

---
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_arrays_e_for/aula1_for.js
@@ -0,0 +1,6 @@
+var numero = 8
+
+for(var i = 1; i <= 9; i++) {
+    console.log("* ")
+    console.log(numero * i)
+}
---
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_arrays_e_for/for_of_exercicio.js
@@ -0,0 +1,11 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida']
+
+for(let nomes of names) {
+    console.log(nomes)
+}
+
+let numeros = [2, 4, 5]
+for(let soma of numeros) {
+    soma += 1
+    console.log(soma)
+}
