6c367c250cd4f45cc32e1a6a1a19247dd900c5cf
joaokruschewsky <joaokruschewsky@gmail.com>
2021-11-29 13:13:20 -0300

    Fim da aula

--- fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/scriptMentoria.js
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/scriptMentoria.js
@@ -1,10 +1,43 @@
-var name1 = 'Gabriel'
-let name2 = 'Rafaela'
-const name3 = 'Otávio'
+//Tipos primitivos e tipagem dinamica
+/*
+const name = "Gabriel"
+const idade = 26
+const isStudent = true
+var xablau //undefined
+var xablau2 = null 
 
-name1 = 'Janiele'
-name2 = 'Luan'
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof isStudent)
+console.log(typeof xablau)
+console.log(typeof xablau2)
+*/ 
+// const isValid = false
+// if(isValid) {
+//     console.log('Valor valido')
+// } else {
+//     console.log('Não é valido')
+// }
 
-console.log(name1)
-console.log(name2)
-console.log(name3)
+// const idade = 21
+// if(idade >= 18) {
+//     console.log('Maior de idade')
+// } else {
+//     console.log('Menor de idade')
+//
+
+
+// const detalhes = true
+// if(!detalhes) {
+//     console.log('Não mostrou os detalhes')
+// } else {
+//     console.log('Não mostra')
+// 
+
+// const name = 'Gean'
+// if(name === 'Douglas' || name === 'Rods') {
+//     console.log('Esse é um bom nome')
+//
+
+//Number, String, Boolean, Undefined, null, Objects.
+// 0 = false.  String vazia '' é false.
