1cdae25e6a6cdbb1a8cc864f330af62322c6706f
Joao Kruschewsky <joaokruschewsky@gmail.com>
2021-11-30 16:25:01 -0300

    Iniciando algoritmo e logica de programação

---
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_3_javascript_logica_de_programacao_e_algoritmos/algoritmos1.js
@@ -0,0 +1,12 @@
+let fruits = [3, 4, 10,1 ,12] 
+let resposta = 0
+
+for(let i = 0; i < fruits.length; i++) {
+    resposta += fruits[i]
+    }
+if(resposta > 15) {
+        console.log('Resultado é: ' + resposta)
+} else {
+    console.log('Menor que 16')
+}
+//console.log(resposta)
