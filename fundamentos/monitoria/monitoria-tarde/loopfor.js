3962ae227c242fc641128faf78e88caa989426f3
Joao Kruschewsky <joaokruschewsky@gmail.com>
2021-11-30 13:53:22 -0300

    Mentoria zeze tarde

---
+++ fundamentos/monitoria/monitoria-tarde/loopfor.js
@@ -0,0 +1,6 @@
+let array = [1, 2, 3, 4, 5]  //length=5    -  4indices
+//[ponto de partida];[condição de parada];[andamento]
+for(let index = 0; index < array.length; index++){
+    const element = array[index]
+    console.log(element)
+}
