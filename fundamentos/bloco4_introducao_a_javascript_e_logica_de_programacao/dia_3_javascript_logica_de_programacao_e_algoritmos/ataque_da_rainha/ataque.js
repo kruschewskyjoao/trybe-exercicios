746afa8cf9dda22df9a5aa2fbfbdbe53408db964
Joao Kruschewsky <joaokruschewsky@gmail.com>
2021-11-30 17:26:56 -0300

    Verifica a diagonal superior direita

--- fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_3_javascript_logica_de_programacao_e_algoritmos/ataque_da_rainha/ataque.js
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_3_javascript_logica_de_programacao_e_algoritmos/ataque_da_rainha/ataque.js
@@ -1,4 +1,5 @@
 
+//linha 6 e coluna 4            x = 6 e y= 4
 //Horizontal é linha  =RowPosition
 //Vertical é coluna
 //posição da rainha
@@ -19,4 +20,24 @@ opponentColumnPosition === queenColumnPosition) {
     canAttack = true
 }
 
-console.log('Pode atacar?', canAttack)
+ //console.log('Pode atacar?', canAttack)
+
+
+//Diagonal Superior Direita ( inicia 6,4 vai para final 8,6) x=8 y=6
+for(let iSupDir = 1; iSupDir < 8; iSupDir++){
+    let currentQueenRow = queenRowPosition + iSupDir        //A localização da rainha(atual) + a localização depois de um passo (um em um)
+    let currentQueenColumn = queenColumnPosition + iSupDir
+
+    //Se a linha for > 8 OU a coluna for > 8 eu tenho que parar o loop
+    if(currentQueenRow > 8 || currentQueenColumn > 8) {
+        break;
+    }
+
+    if(currentQueenRow === opponentRowPosition || currentQueenColumn === opponentColumnPosition){       //Se o oponente estiver no meio do caminho, ele para
+        canAttack = true
+        console.log('Pode atacar na diagonal superior direita')
+        break
+    }
+
+    console.log(currentQueenRow, currentQueenColumn)
+}
