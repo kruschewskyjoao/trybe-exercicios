ac139c5387540716599aea42c2a72decf5c840cd
Joao Kruschewsky <joaokruschewsky@gmail.com>
2021-12-01 16:45:18 -0300

    Exemplos com objetos, forOf e forIn

---
+++ fundamentos/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_4_javascript_objetos_e_funcoes/aulaRods.js
@@ -0,0 +1,48 @@
+let person = {
+  name: 'Joana',
+  lastName: 'Silva',
+  age: 30,
+  canDrive: true,
+  favoriteMovies: ['Star Wars', 'Batman'],
+  adress: {
+    street: 'Rua XYZ',
+    number: 123,
+    city: 'São Paulo'
+  }
+}
+//console.log(person)
+
+//Notação por ponto(dot notation)
+//console.log(person.age)
+//console.log(person.adress)
+
+//Notação por colchetes
+//console.log(person['age'])
+
+//person['favoriteFood'] = 'Pizza'  //person.favoriteFood = 'pizza' também funciona
+//console.log(person)
+
+// console.log(person.adress.city)
+// console.log(person.adress['street'])
+// console.log(person['adress']['number'])
+
+// delete person.lastName  // Omite a propriedade
+// console.log(person)
+
+
+//for normal
+let myArray = [6, 5, 4, 3, 2, 1]
+for(let i = 0; i < 6; i++) {
+  console.log('Com for convencional: ' + myArray[i])
+}
+
+// for/of   -- Problemas mais simples.
+for(let arr of myArray) {
+  console.log('Com for/of ' + arr)
+}
+
+//for/in   - Ele pega o indice
+for(let arr in myArray) {
+  console.log('Com for/in ' + arr)
+  console.log(myArray[arr]) //pega o valor no indice
+}
